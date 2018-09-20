dc41a0095c9e90f449215544fefe9638a3f9374f
Lightvvind <pihl.jacob@gmail.com>
2018-09-20 20:27:16 +0200

    Added parser class

---
+++ SimpleParser.py
@@ -0,0 +1,12 @@
+from html.parser import HTMLparser
+from urllib import parse
+
+class SimpleParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
--- webscraper.py
+++ webscraper.py
@@ -1,1 +1,15 @@
 import os
+from SimpleParser import SimpleParser
+
+def get_queue():
+	temp = open("queue.txt", 'r')
+	scrape_queue = temp.read().splitlines()
+	temp.close()
+	return scrape_queue
+
+if __name__ == "__main__":
+	scrape_queue = get_queue()
+
+	for website in scrape_queue:
+		# Create text file for tree view
+		# Create empty completed list for already visited links
