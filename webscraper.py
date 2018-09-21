2342fb1c583936026f376b7c3ad42ccb2bf73e82
Lightvvind <pihl.jacob@gmail.com>
2018-09-21 18:40:47 +0200

    Fixed some bugs

--- webscraper.py
+++ webscraper.py
@@ -6,10 +6,13 @@ import urllib.request
 current_domain = ""
 visited_links = []
 cur_file = None
+total_pages_crawled = 0
+maxdepth = 5
+starting_depth = 0
 
 def create_file(name):
-	file = open(name + ".txt", 'w+', buffering=1)
-	return file
+	global cur_file
+	cur_file = open(name + ".txt", 'w+', buffering=1)
 
 def close_file():
 	cur_file.close()
@@ -31,7 +34,7 @@ def get_html(url):
 
 def get_domain(link):
 	parsed_domain = urlparse(link)
-	domain = parsed_domain.netloc or parsed_domain.path # Just in case, for urls without scheme
+	domain = parsed_domain.netloc # or parsed_domain.path # Just in case, for urls without scheme
 	domain_parts = domain.split('.')
 	if len(domain_parts) > 2:
 		return str('.'.join(domain_parts[-(2 if domain_parts[-1] in {
@@ -45,6 +48,7 @@ def get_queue():
 	return scrape_queue
 
 def handle_links(links, depth, maxdepth):
+	global total_pages_crawled
 	# Stop printing stuff when we reach maxdepth
 	if depth > maxdepth:
 		return
@@ -54,11 +58,18 @@ def handle_links(links, depth, maxdepth):
 		parser.cur_links = []
 		new_links = []
 
+		if cur_link_domain == "":
+			cur_link_domain = current_domain
+			if cur_link[:1] != "/":
+				cur_link = "/" + cur_link
+			cur_link = "http://" + current_domain + cur_link
+
 		# If the current link is not the same domain as the website -> end
 		if (cur_link in visited_links):
 			continue
 
 		# Write to file with current depth as indent
+		print("Total pages crawled: " + str(total_pages_crawled))
 		write_to_file(cur_link, depth)
 
 		# If the current link has not been visited AND it is part of the same website
@@ -72,25 +83,26 @@ def handle_links(links, depth, maxdepth):
 		visited_links.append(cur_link)
 
 		# Call handle links again, with increased depth
+		total_pages_crawled += 1
 		handle_links(new_links, depth+1, maxdepth)
 
 if __name__ == "__main__":
 	scrape_queue = get_queue()
 	parser = SimpleParser()
-	maxdepth = 5
-	starting_depth = 0
 
 	for website in scrape_queue:
 		if website[:7] != "http://":
 			website = "http://" + website;
+
+		total_pages_crawled = 0
 		visited_links = []
 
 		# Get current domain and print to terminal
 		current_domain = get_domain(website)
-		print("\nCurrently parsing: " + current_domain)
+		print("\nCurrently crawling: " + current_domain)
 
 		# Create new file for writing to
-		cur_file = create_file(current_domain)
+		create_file(current_domain)
 		write_to_file(website, starting_depth)
 
 		# Get html links
@@ -98,6 +110,7 @@ if __name__ == "__main__":
 		parser.cur_links = []
 		parser.feed(website_HTML)
 		website_links = parser.cur_links
+		total_pages_crawled += 1
 
 		visited_links.append(website)
 
@@ -105,4 +118,4 @@ if __name__ == "__main__":
 		handle_links(website_links, starting_depth, maxdepth)
 
 		close_file()
-		print("Done parsing: " + current_domain)
+		print("Done crawling: " + current_domain)
